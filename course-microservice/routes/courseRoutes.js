77d4895a857c1bdbb23ab514929fd1d2246fc26d
udanthaisuru@gmail.com <udanthaisuru@gmail.com>
2024-09-24 20:01:47 +0530

    xss - update courseRoutes

--- course-microservice/routes/courseRoutes.js
+++ course-microservice/routes/courseRoutes.js
@@ -143,8 +143,26 @@ router.put("/update/:courseId", async (req, res) => {
       return res.status(404).send({error: "Course not found"});
     }
 
+    const sanitizedCourse = {
+      title: sanitize(course.InstructorId),
+      description: sanitize(course.CourseName),
+      content: sanitize(course.instructor),
+      content: sanitize(course.description),
+      content: sanitize(course.duration),
+      content: sanitize(course.level),
+      content: sanitize(course.price),
+      content: sanitize(course.lectureNotes),
+      content: sanitize(course.status),
+      content: sanitize(course.lectureVideos),
+      content: sanitize(course.preview),
+      content: sanitize(course.lessons),
+      content: sanitize(course.totalLessons),
+    };Í
+
     // If the course exists, send it as a response
-    res.status(200).send({message: "Course updated successfully", course});
+    res
+      .status(200)
+      .send({message: "Course updated successfully", sanitizedCourse});
   } catch (error) {
     res.status(500).send({error: "Error updating course"});
   }
